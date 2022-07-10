1649786719eaab62a7b7b15981d48a4fd93ef74d
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2022-07-11 00:49:23 +0200

    add first two input

--- src/routes/skilltree/index.svelte
+++ src/routes/skilltree/index.svelte
@@ -12,6 +12,18 @@
 
   <div class="w-full px-1">
     <SectionHeader>Allgemeines</SectionHeader>
+    <div class="flex border-2 border-sky-700 w-full text-xs rounded-md mt-2">
+      <label class="shrink bg-white text-blue-300 p-1 rounded-l-md" for=""
+        >Beschreibung-Inline</label
+      >
+      <input class="grow bg-blue-100 p-1 rounded-r-md" type="text" />
+    </div>
+
+    <div class="flex flex-col border-2 border-sky-700 w-full text-xs rounded-md mt-2">
+      <label class="shrink bg-white text-blue-300 p-1 rounded-t-md" for="">Beschreibung-Above</label
+      >
+      <input class="grow bg-blue-100 p-1 rounded-b-md" type="text" />
+    </div>
   </div>
   <div class="w-full lg:w-1/3 px-1">
     <SectionHeader>Aspekte</SectionHeader>
