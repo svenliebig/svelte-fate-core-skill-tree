5235c5198a995a32dca403af7482323b0527b9a1
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2022-07-10 15:39:58 +0300

    add skilltree page

---
+++ src/routes/skilltree/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+  <title>Fate Core Skilltree</title>
+  <meta name="description" content="A Fate Core Skilltree Implementation" />
+</svelte:head>
+
+<div class="skilltree">
+  <h1>Skilltree</h1>
+
+  <div class="p-6 w-full mx-auto bg-white dark:bg-slate-800 rounded-xl shadow-lg flex items-center space-x-4">
+    Hello world
+  </div>
+</div>
+
+<style>
+</style>
