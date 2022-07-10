22000cbcce6989853fafd9d6ab3388997576b0bb
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2022-07-10 23:47:10 +0200

    add flex wraps

--- src/routes/skilltree/index.svelte
+++ src/routes/skilltree/index.svelte
@@ -7,7 +7,7 @@
   <meta name="description" content="A Fate Core Skilltree Implementation" />
 </svelte:head>
 
-<div class="skilltree flex -mx-1">
+<div class="skilltree flex -mx-1 flex-wrap">
   <h1 class="sr-only">Skilltree</h1>
 
   <div class="w-full px-1">
@@ -25,7 +25,7 @@
   <div class="w-full lg:w-1/2 px-1">
     <SectionHeader>Stunts</SectionHeader>
   </div>
-  <div class="w-full sm:w-1/2 lg:w-1/3 flex flex-row px-1">
+  <div class="w-full sm:w-1/2 lg:w-1/3 flex flex-col px-1">
     <div class="w-full">
       <SectionHeader>Körperlicher Stress (Kraft)</SectionHeader>
     </div>
