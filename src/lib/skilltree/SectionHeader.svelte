9b0a90b288ccf9b511d2c6600c81cf4cead08fdd
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2022-07-10 23:43:38 +0200

    refactor margins and paddings

--- src/lib/skilltree/SectionHeader.svelte
+++ src/lib/skilltree/SectionHeader.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <h2
-  class="border-sky-100 text-sky-100 shadow-md rounded-md p-1 text-xs shadow-sky-700 border-2 bg-sky-600 uppercase"
+  class="border-sky-100 text-sky-100 shadow-md rounded-md p-1 mt-2 text-xs shadow-sky-700 border-2 bg-sky-600 uppercase"
 >
   <slot />
 </h2>
--- src/routes/skilltree/index.svelte
+++ src/routes/skilltree/index.svelte
@@ -7,33 +7,33 @@
   <meta name="description" content="A Fate Core Skilltree Implementation" />
 </svelte:head>
 
-<div class="skilltree flex">
+<div class="skilltree flex -mx-1">
   <h1 class="sr-only">Skilltree</h1>
 
-  <div class="w-full mt-3">
+  <div class="w-full px-1">
     <SectionHeader>Allgemeines</SectionHeader>
   </div>
-  <div class="w-full lg:w-1/3 mt-3 lg:pr-3">
+  <div class="w-full lg:w-1/3 px-1">
     <SectionHeader>Aspekte</SectionHeader>
   </div>
-  <div class="w-full lg:w-2/3 mt-3">
+  <div class="w-full lg:w-2/3 px-1">
     <SectionHeader>Fertigkeiten</SectionHeader>
   </div>
-  <div class="w-full lg:w-1/2 mt-3 lg:pr-3">
+  <div class="w-full lg:w-1/2 px-1">
     <SectionHeader>Extras</SectionHeader>
   </div>
-  <div class="w-full lg:w-1/2 mt-3">
+  <div class="w-full lg:w-1/2 px-1">
     <SectionHeader>Stunts</SectionHeader>
   </div>
-  <div class="w-full sm:w-1/2 lg:w-1/3 mt-3 sm:pr-3 flex flex-row">
+  <div class="w-full sm:w-1/2 lg:w-1/3 flex flex-row px-1">
     <div class="w-full">
       <SectionHeader>Körperlicher Stress (Kraft)</SectionHeader>
     </div>
-    <div class="w-full mt-3">
+    <div class="w-full">
       <SectionHeader>Gestiger Stress (Wille)</SectionHeader>
     </div>
   </div>
-  <div class="w-full sm:w-1/2 lg:w-2/3 mt-3">
+  <div class="w-full sm:w-1/2 lg:w-2/3 px-1">
     <SectionHeader>Konsequenzen</SectionHeader>
   </div>
 </div>
